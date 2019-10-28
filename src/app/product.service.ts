08812d29fdff7c89cce2ac3427b3cd97cba3cab5
Jesse.Ssempijja <jesse.ssempijja@postbank.co.ug>
2019-10-28 11:23:28 +0300

    Update on product service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
-import {Response, Http} from '@angular/http'
+import { Response, Http } from '@angular/http'
 import 'rxjs/add/operator/map'
 
 @Injectable()
 export class ProductService {
+  private _albumUrl = '../assets/album.json'
 
   constructor(private _http: Http) { }
 
-  private _albumUrl = '../assets/album.json'
-
-  getAlbum(id: number){
-    return this._http.get(this._albumUrl).map(response=>{
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map(response => {
       response.json()
     })
   }
